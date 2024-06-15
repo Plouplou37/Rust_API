8e5470f832b825c7ef8e97314b5ab19ea64d03a5
Plouplou37 <quentin.plourdeau@gmail.com>
2024-06-15 12:11:58 +0800

    Add genercis module

---
+++ intermediate-concepts/src/generics.rs
@@ -0,0 +1,53 @@
+/*enum Option<T> {
+    Some(T),
+    None,
+}*/
+
+use std::process::Output;
+
+fn option_example(x: i32) -> Option<String> {
+    match x > 2 {
+        true => Some(String::from("result")),
+        false => None,
+    }
+}
+
+/*enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}*/
+enum Custom<T> {
+    EXAMPLE(T),
+    SAMPLE(T),
+}
+
+struct Rectangle<T> {
+    height: T,
+    width: T,
+}
+
+struct Cube<T> {
+    height: T,
+    width: T,
+    lenght: T,
+}
+
+fn struct_example() {
+    let r1 = Rectangle {
+        height: 1,
+        width: 5,
+    };
+    let c1 = Cube {
+        height: 1,
+        width: 5,
+        lenght: 10,
+    };
+}
+
+fn mul_of_number<T: std::ops::Mul<Output = T>>(num1: T, num2: T) -> T {
+    num1 * num2
+}
+
+fn main() {
+    println!("{}", mul_of_number(2, 10))
+}
